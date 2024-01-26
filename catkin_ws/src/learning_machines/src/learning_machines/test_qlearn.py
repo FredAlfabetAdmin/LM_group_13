c1652d0304974c354fc6d2b1fb1b001414e13db4
sandor111 <sandorptjr1@gmail.com>
2024-01-26 13:46:04 +0100

    Getting image from rob results in timeout?

--- catkin_ws/src/learning_machines/src/learning_machines/test_qlearn.py
+++ catkin_ws/src/learning_machines/src/learning_machines/test_qlearn.py
@@ -18,6 +18,57 @@ from torch import optim, nn
 import torch, time, random
 import numpy as np
 import joblib
+import cv2
+
+
+class Blob_Detection():
+    def __init__(self, camera_width: int, camera_height: int, dark: bool = False) -> None:
+        self.camera_width = camera_width
+        self.camera_height = camera_height
+
+        self.params = cv2.SimpleBlobDetector_Params()
+        self.params.filterByColor = True
+        self.params.blobColor = 0 if dark else 255
+        self.params.filterByCircularity = True
+        self.params.minCircularity = 0.6
+        self.params.filterByConvexity = True
+        self.params.minConvexity = 0.9
+        self.params.filterByInertia = True
+        self.params.minInertiaRatio = 0.6
+        self.detector = cv2.SimpleBlobDetector_create(self.params)
+
+    def blob_detect(self, rob: IRobobo):
+        frame = rob.get_image_front()
+
+        # TODO:resize if needed
+        frame = cv2.resize(frame, (self.camera_width, self.camera_height))
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        lower_green = np.array([40, 40, 40])
+        upper_green = np.array([80, 255, 255])
+
+        # mask for the green color
+        green_mask = cv2.inRange(hsv_frame, lower_green, upper_green)
+
+        # AND operation to get the green regions
+        green_regions = cv2.bitwise_and(frame, frame, mask=green_mask)
+
+        gray_frame = cv2.cvtColor(green_regions, cv2.COLOR_BGR2GRAY)
+
+        # perform blob detection
+        keypoints = self.detector.detect(gray_frame)
+
+        cv2.imwrite(str("./frame.png"), frame)
+        cv2.imwrite(str("./gray_frame.png"), gray_frame)
+
+        x, y = 0, 0.5
+        size_percent = gray_frame[gray_frame > 0.5].shape[0] / (gray_frame.shape[0] * gray_frame.shape[1]) * 100
+
+        if keypoints:
+            keypoint = keypoints[0]
+            x, y = int(keypoint.pt[0]) / self.camera_width, int(keypoint.pt[1]) / self.camera_height
+            # size_percent = (keypoint.size / (self.camera_width * self.camera_height)) * 100
+            # x and y values along with the percentage of blob area
+        return [x, y, size_percent]
 
 
 class QLearningAgent:
@@ -28,8 +79,7 @@ class QLearningAgent:
         self.exploration_prob = exploration_prob
 
         # Q-table to store Q-values for each state-action pair
-        self.q_table = {((0, 0, 0, 0, 0), 0): 16.751523737780296, ((0, 0, 0, 0, 0), 1): 12.33398376013057, ((0, 0, 0, 0, 0), 2): 13.210192112780803, ((0, 0, 1, 0, 0), 0): 1.4111746196753698, ((0, 0, 1, 1, 0), 2): 0.0, ((1, 1, 0, 0, 0), 0): 2.3698431988910547, ((1, 1, 1, 0, 0), 0): 0.14211064545445848, ((1, 1, 1, 0, 0), 1): 0.03384014256515325, ((0, 1, 1, 1, 1), 0): 0.0, ((0, 0, 1, 1, 1), 0): 0.1792741161317035, ((0, 0, 1, 1, 1), 1): 6.497725653215419, ((0, 0, 0, 1, 1), 1): 6.851379649808463, ((0, 0, 0, 0, 1), 0): 1.3592966072185944, ((0, 0, 0, 0, 1), 1): 16.603345330537916, ((0, 0, 1, 0, 0), 1): 1.7071473273287496, ((0, 0, 0, 0, 1), 2): 0.3705698642679427, ((0, 0, 1, 1, 0), 0): 0.2, ((0, 1, 1, 1, 1), 1): 2.402990722215207, ((1, 0, 1, 0, 0), 0): 0.2, ((1, 1, 1, 1, 1), 0): 0.06488910597268528, ((1, 1, 1, 1, 1), 1): 3.0578588810104046, ((1, 1, 1, 1, 1), 2): 0.04753621342357239, ((0, 1, 1, 1, 0), 0): 0.2647834452515325, ((1, 1, 1, 1, 0), 0): 0.015795378413981424, ((1, 1, 1, 1, 0), 2): 0.8804266189601633, ((1, 1, 1, 0, 0), 2): 3.7548767236494656, ((1, 1, 0, 0, 0), 2): 0.4, ((1, 0, 0, 0, 0), 0): 2.5953310076429, ((1, 1, 0, 0, 0), 1): 0.0, ((1, 1, 1, 0, 1), 0): 0.24586904520305, ((1, 0, 0, 0, 0), 2): 5.8891548005517205, ((0, 0, 0, 1, 0), 0): 1.2488652621157428, ((1, 0, 0, 0, 1), 0): 0.7742486169514946, ((1, 1, 1, 0, 1), 2): 0.0, ((1, 0, 1, 1, 1), 0): 0.29042125853847106, ((0, 0, 1, 1, 1), 2): 0.15373311170223558, ((0, 0, 1, 0, 0), 2): 0.4179230102659894}
-
+        self.q_table = {}
     def get_q_value(self, state, action):
         return self.q_table.get((state, action), 0.0)
 
@@ -54,11 +104,12 @@ class QLearningAgent:
         self.q_table[(state, action)] = new_q
 
 
-def get_current_state(scaler, irs):
-    front_sensors, back_sensors = scale_and_return_ordered(scaler, irs)
-    print(front_sensors)
+def get_current_state(scaler, rob):
+    green_perc = Blob_Detection(640, 480).blob_detect(rob)[-1]
+    front_sensors, back_sensors = scale_and_return_ordered(scaler, rob.read_irs())
+    #print(front_sensors)
     state = tuple(1 if value > -0.3 else 0 for value in front_sensors)
-    return state
+    return state + (green_perc,)
 
 
 def scale_and_return_ordered(scaler, irs):
@@ -110,7 +161,7 @@ def move_robobo_and_calc_reward(scaler, action, rob, state):
             print("Turning right")
             movement = [20, -20, 250]
         rob.move_blocking(int(movement[0]), int(movement[1]), int(movement[2]))
-    next_state = get_current_state(scaler, rob.read_irs())
+    next_state = get_current_state(scaler, rob)
 
     # If robobo turned away from object
     if sum(state) > sum(next_state) and action != 0:
@@ -129,7 +180,7 @@ def run_qlearning_classification(rob: IRobobo):
     if not isinstance(rob, SimulationRobobo):
         agent = QLearningAgent(num_actions, exploration_prob=0)
         scaler = joblib.load('hardware_powertrans_scaler.gz')
-        state = get_current_state(scaler, rob.read_irs())
+        state = get_current_state(scaler, rob)
         while True:  # Take max 75 steps per round
             action = agent.choose_action(state)
 
@@ -152,7 +203,7 @@ def run_qlearning_classification(rob: IRobobo):
             rob.set_position(Position((random.random()*2.4)-1.2, (random.random()*2.4)-1.2, 0.03), Orientation(-90, -90, -90))
             rob.set_target_position(Position((random.random()*2.4)-1.2, (random.random()*2.4)-1.2, 0.2))
         rob.play_simulation()
-        state = get_current_state(scaler, rob.read_irs())  # Replace with your function to get the current state
+        state = get_current_state(scaler,rob)  # Replace with your function to get the current state
         for step in range(240):  # Take max 75 steps per round
             action = agent.choose_action(state)
 
